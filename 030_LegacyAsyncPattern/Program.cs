1facfd8b41d6a018e220d779387c322c30fae600
Dzmitry Kliuchavy <klutch1991@yandex.ru>
2019-03-19 17:44:55 +0300

    + added new samples

---
+++ 030_LegacyAsyncPattern/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+
+namespace _030_LegacyAsyncPattern
+{
+	class Program
+	{
+		// not supported on core
+		static void Main(string[] args)
+		{
+			Func<int, int, int> myDelegate = new Func<int, int, int>(HardSum);
+
+			// Under the hood, it creates a ManualResetEvent 
+			// and returns it wrapped into some IAsyncResult
+			// implementation
+			IAsyncResult asyncResult = myDelegate.BeginInvoke(2, 2, null, null);
+
+			// awaits until sum method will finish it's work
+			// uses resetEvent.WaitOne()
+			int result = myDelegate.EndInvoke(asyncResult);
+
+			Console.WriteLine("Ended invocation");
+		}
+
+		static int HardSum(int a, int b)
+		{
+			Console.WriteLine($"Executing sum. ThreadPoolThread: {Thread.CurrentThread.IsThreadPoolThread}");
+			Thread.Sleep(1000);
+			return a + b;
+		}
+	}
+}
